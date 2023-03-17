c5739e2edbb51db5ad5da695c69e2f027a28df4f
Adeel-works <72549306+Adeel-works@users.noreply.github.com>
2023-03-17 17:23:37 +0500

    added yield

--- src/components/TabularView.vue
+++ src/components/TabularView.vue
@@ -10,6 +10,13 @@
       <div class="d-flex m-1" :key="item" v-for="item in years">
         <b-button :variant="selectedYears.find(i=>i==item) ? 'primary':'secondary'" @click="()=>handleYears(item)"> {{ item }} Yrs </b-button>
       </div>
+      <div style="width: 50px" />
+
+      <div class="d-flex m-1" :key="item" v-for="item in filters">
+        <b-button @click="()=>handleSelectedFilter(item)">
+          {{ item.label }}
+        </b-button>
+      </div>
     </div>
     <div class="mb-3 mt-5 col-3">
       <b-form-input
@@ -20,11 +27,12 @@
     </div>
     <div class="tableContainer">
     <div class="d-flex justify-content-between">
-      <div class="d-flex">
-     <h5 class="headingTitle">date sent</h5>
+      <!-- <div class="d-flex">
+     <h5 class="headingTitle">DATE SENT</h5>
      <div style="width:10px"/>
-     <h5 class="headingTitle">company</h5>
-    </div>
+     <h5 class="headingTitle">COMPANY</h5>
+    </div> -->
+
     <!-- <div class="d-flex">
      <div v-for="items in years" :key="items">
       <div class="d-flex flex-column ">
@@ -43,47 +51,89 @@
   </div>
      </div> -->
     </div>
+    <!-- <div class="borderClass"/> -->
 
     <div class=" justify-content-between"> 
-      <div  v-for="items in results" :key="items">
+      <div class="d-flex" >
+        <div style="width:40px"/>
+        
+        <h5 class="headingTitle">DATE SENT</h5>
+        <div style="width:10px"/>
+        <h5 class="headingTitle">COMPANY</h5>
+       </div>
+      <div   v-for="(items,index) in results" :key="items">
       <div class="d-flex align-items-start">
      <!-- <h5 class="headingTitle mt-2">{{ items.DateSent }}</h5> -->
      <!-- <div style="width:50px"/> -->
      <!-- <h5 class="headingTitle mt-2">{{ items.Company }}</h5> -->
      <div class="d-flex flex-column">
+     <!-- <h5 class="headingTitle mt-2">{{  }}</h5> -->
+     <h5 class="headingTitle " style="margin-top: 0.01rem;">{{  }}</h5>
      <h5 class="headingTitle mt-2">{{  }}</h5>
      <h5 class="headingTitle mt-2">{{  }}</h5>
-     <h5 class="headingTitle mt-2">{{  }}</h5>
-     <h5 class="headingTitle mt-2">{{  }}</h5>
+    
+    <div class="borderClass"/>
+
      <div class="d-flex">
       <!-- <div style="width:120px"/> -->
+      <b-button variant="none" @click="()=>toggleInformation(index)">
+          <div v-if="items.isOpen">
+          <b-icon-arrow-down />
+        </div>
+        <div v-else>
+          <b-icon-arrow-up />
+        </div>
+      </b-button>
      <h5 class="headingTitle mt-2">{{ items.DateSent }}</h5>
-
      <h5 style="width:100px" class="headingTitle mt-2">{{ items.Company }}</h5>
     </div>
+    <div v-if="items?.isOpen">
      <h5 class="headingTitle mt-2">{{  }}</h5>
      <div class="d-flex">
-      <div style="width:120px"/>
+      <div style="width:160px"/>
+     <h5 style="width:120px" class="headingTitle mt-2">Spread</h5>
+     
+      </div>
+     <div class="d-flex" style="margin-top: 10px;">
+      <div style="width:160px"/>
+
 
      <h5 style="width:100px" class="headingTitle mt-2">Yield</h5>
      
       </div>
+      
+      <div class="d-flex" style="margin-top: 20px;">
+        <div style="width:160px"/>
+
+     <h5 style="width:120px" class="headingTitle mt-2">3MLSpread</h5>
+     
+      </div>
      </div>
-     <div class="d-flex">
+    </div>
+     <div v-if="items?.isOpen" class="d-flex">
+      
     
-    <div v-for="item in items.Quote" :key="item">
+    <div  v-for="item in items.Quote" :key="item">
       
       <div class="d-flex" v-if="selectedYears.find(i=>i==item.Years)">
       <div class="d-flex flex-column">
-      <p style="padding: 10px">{{ item.Years }} Years</p>
+
+      <p style="padding: 10px;padding-top: 0px;padding-bottom: 0px;">{{ item.Years }} Years</p>
+      <div class="borderClass"/>
+
       <div class="d-flex">
       <div class="d-flex flex-column">
       <p style="padding: 10px">FIX</p>
-      <p style="padding: 10px" v-if="item.CouponType=='FIX'">{{ item.Yield }}</p>
+      <p style="padding: 10px;padding-top: 0px;" v-if="item.CouponType=='FIX'">{{ item.Spread }}</p>
+      <p style="padding: 10px;padding-top: 0px;" v-if="item.CouponType=='FIX'">{{ item.Yield }}</p>
+      <p style="padding: 10px;padding-top: 12px;" v-if="item.CouponType=='FIX'">{{ item['3MLSpread'] }}</p>
+  
      </div>
      <div class="d-flex flex-column">
-      <p style="padding: 10px">FRN</p>
-      <p style="padding: 10px" v-if="item.CouponType=='FRN'">{{ item.Yield }}</p>
+      <p style="padding: 10px;">FRN</p>
+      <p style="padding: 10px;padding-top: 0px;" v-if="item.CouponType=='FRN'">{{ item.Spread }}</p>
+      <p style="padding: 10px;padding-top: 0px;" v-if="item.CouponType=='FRN'">{{ item.Yield }}</p>
+      <p style="padding: 10px;padding-top: 12px;" v-if="item.CouponType=='FRN'">{{ item['3MLSpread'] }}</p>
       </div>
     </div>
       </div> 
@@ -158,18 +208,24 @@
         </template>
       </b-table>
     </div> -->
+    <div class="d-flex p-3" >
+    <h5>Averge by spread</h5>
   </div>
+  
+  </div>
+  
 </template>
 
 <script>
-import {  BButton, BFormInput} from "bootstrap-vue";
+import {  BButton, BFormInput,BIconArrowDown,BIconArrowUp} from "bootstrap-vue";
 import data from "../../DATA.json";
 
 export default {
   components: {
     
-   
+    BIconArrowDown,
     BFormInput,
+    BIconArrowUp,
     BButton,
   },
   data() {
@@ -177,6 +233,7 @@ export default {
       sortDesc: false,
       selectedCurrency: "",
       selectedYears:[],
+      selectedFilter:'',
       searchQuery: "",
       fields: [
         { key: "DateSent", sortable: true, label: "DATE SENT" },
@@ -184,10 +241,13 @@ export default {
         { key: "FIX", sortable: false, label: "FIX" },
         { key: "FRN", sortable: false, label: "FRN" },
       ],
-      items: data.Items,
+      items: this.data.Items,
     };
   },
   methods: {
+    handleSelectedFilter(item){
+        this.selectedFilter=item
+    },
     handleYears(item){
       console.log(item)
       if(this.selectedYears.find(i=>i==item)){
@@ -197,17 +257,21 @@ export default {
         this.selectedYears.push(item)
       }
     },
+    toggleInformation(index){
+      console.log('this',index,this.items[index]);
+      this.items[index].isOpen = !this.items[index].isOpen
+
+    },
     handleSearch(text) {
-      console.log("text", text);
-      console.log(this.currencies);
       this.searchQuery = text;
     },
     handleCurrency() {},
   },
   created() {
-    console.log(data.Items);
+   console.log('temp',this.temp)
   },
   updated() {
+    console.log(this.results);
   },
   computed: {
     results() {
@@ -217,8 +281,16 @@ export default {
         return this.items;
       }
     },
+    filters(){
+      if(this.data){
+        return [{id:'spread',label:'Spread'},{id:'yield',label:'Yield'},{id:'3ml',label:'3MLSpread'}]
+      }else{
+        return []
+      }
+    },
+    
     years() {
-      if (data) {
+      if (this.data) {
         const temp = [...data.Items];
         const years = [];
         temp.forEach((i) => {
@@ -227,18 +299,14 @@ export default {
               years.push(x.Years);
             });
           }
-        });
-        // if(this.selectedYears)
-    
-        
-
+        });    
         return [...new Set(years)].sort((x,y)=>x-y);
       }else{
         return []
       }
     },
     currencies() {
-      if (data) {
+      if (this.data) {
         const temp = [...data.Items];
         const currencies = [];
         temp.forEach((i) => {
@@ -259,10 +327,17 @@ export default {
       } else return [];
     },
   },
-  setup() {
- 
+  setup(){
+    // console.log('hello')
 
-  },
+    data.Items.forEach(i=>{
+      i.isOpen=true
+    })
+    
+    return {
+      data,
+    }
+  }
 };
 </script>
 
@@ -297,8 +372,14 @@ export default {
   padding-left: 0px;
 }
 .tableContainer{
-  border: 1px solid rgb(182, 182, 182);
+  /* border: 1px solid rgb(182, 182, 182); */
   padding: 10px;
+  margin-top: 50px;
   border-radius: 8px;
+}
+.borderClass{
+  height: 1px;
+  background-color: grey;
+  width: 100%;
 }
 </style>
